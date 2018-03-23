7e9e7882f73d400eb8b06bf5418167fdd0c16d92
tamassneider <tamassneider90@gmail.com>
2018-03-23 13:37:40 +0100

    First version of application moved into github

---
+++ models/author.js
@@ -0,0 +1,50 @@
+var mongoose = require('mongoose');
+var moment = require('moment');
+
+var Schema = mongoose.Schema;
+var AuthorSchema = new Schema(
+    {
+        first_name: {type: String, required: true, max: 100},
+        family_name: {type: String, required: true, max: 100},
+        date_of_birth: {type: Date},
+        date_of_death: {type: Date},
+    }
+);
+
+// Virtual for author's full name
+AuthorSchema
+.virtual('name')
+.get(function () {
+  return this.family_name + ', ' + this.first_name;
+});
+
+// Virtual for author's URL
+AuthorSchema
+.virtual('url')
+.get(function () {
+    return '/catalog/author/' + this._id;
+});
+
+// Virtual for formatted birth date
+AuthorSchema
+.virtual('date_of_birth_formatted')
+.get(function () {
+  return this.date_of_birth ? moment(this.date_of_birth).format('YYYY-MM-DD') : '';
+});
+
+// Virtual for formatted death date
+AuthorSchema
+.virtual('date_of_death_formatted')
+.get(function () {
+  return this.date_of_death ? moment(this.date_of_death).format('YYYY-MM-DD') : '';
+});
+
+// Virtual for lifespan
+AuthorSchema
+.virtual('lifespan')
+.get(function() {
+    return this.date_of_birth ? moment(this.date_of_birth).format('YYYY-MM-DD') : '' + ' - ' + this.date_of_death ? moment(this.date_of_death).format('YYYY-MM-DD') : '';
+})
+
+// Export model
+module.exports = mongoose.model('Author', AuthorSchema);
---
+++ models/bookinstance.js
@@ -0,0 +1,38 @@
+var mongoose = require('mongoose');
+var moment = require('moment');
+
+var Schema = mongoose.Schema;
+
+var BookInstanceSchema = new Schema(
+  {
+    book: { type: Schema.ObjectId, ref: 'Book', required: true }, //reference to the associated book
+    imprint: {type: String, required: true},
+    status: {type: String, required: true, enum: ['Available', 'Maintenance', 'Loaned', 'Reserved'], default: 'Maintenance'},
+    due_back: {type: Date, default: Date.now}
+  }
+);
+
+// Virtual for bookinstance's URL
+BookInstanceSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/bookinstance/' + this._id;
+});
+
+//Virtual for formatted date
+BookInstanceSchema
+.virtual('due_back_formatted')
+.get(function () {
+  return moment(this.due_back).format('MMMM Do, YYYY');
+});
+
+//Other Virtual for formatted date YYYY-MM-DD
+BookInstanceSchema
+.virtual('due_back_YYYY-MM-DD')
+.get(function () {
+  return moment(this.due_back).format('YYYY-MM-DD');
+});
+
+
+//Export model
+module.exports = mongoose.model('BookInstance', BookInstanceSchema);
